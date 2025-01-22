4c222708ec6c4dcc4ec88b8383bf31d7dfc0d91c
vaiibbbhav <vaibhavsharmas021@gmail.com>
2025-01-22 17:14:38 +0530

    Add user authentication pages and update routing in App component

--- Rubix/src/App.tsx
+++ Rubix/src/App.tsx
@@ -1,14 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import useAuth from "./hook/useAuth"
 import MainLayout from "./layout/MainLayout";
+import HackathonInfo from "./Pages/HackathonInfo/HackathonInfo";
+import UserLogin from "./Pages/Auth/UserLogin";
+import UserRegister from "./Pages/Auth/UserRegister";
+
+export const baseUrl = "https://live-merely-drum.ngrok-free.app"
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<h1>register ho gaya bhaiiiiiiiiiiiiiiiiiiiiiiiiiiii</h1>} />
         <Route path="/chat" element={<h1>chat</h1>} />
+        <Route path="/hackathonInfo" element={<HackathonInfo/>} />
+        <Route path="/userLogin" element={<UserLogin/>} />
+        <Route path="/userRegister" element={<UserRegister/>} />
+
       </Route>
     </Routes>
   );
