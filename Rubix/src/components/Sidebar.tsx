2e51658f6b985c98271bb33e754fac3a2d2a199e
yanshuman <yanshuman2005@gmail.com>
2025-01-24 11:17:29 +0530

    Refactor Sidebar and DashboardLayout components for improved layout and styling

--- Rubix/src/components/Sidebar.tsx
+++ Rubix/src/components/Sidebar.tsx
@@ -1,119 +1,128 @@
 import { IconBaseProps } from "react-icons";
-import { Link, NavLink} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { CodeXml, ListTodo, LogOut, Settings, User } from "lucide-react";
 import Logo from "../assets/logo.png";
 import { TbLogout2 } from "react-icons/tb";
 
 const navItems = [
-  {
-    path: "/dashboard/profile",
-    label: "Profile",
-    icon: (props: IconBaseProps) => <User {...props} />,
-  },
-  {
-    path: "/dashboard/hackathons",
-    label: "Hackathons",
-    icon: (props: IconBaseProps) => <CodeXml {...props} />,
-  },
-  {
-    path: "/dashboard/organizehackathons",
-    label: "Organize a Hackathon",  
-    icon: (props: IconBaseProps) => <ListTodo {...props} />,
-  },
-  {
-    path: "/",
-    label: "Settings",  
-    icon: (props: IconBaseProps) => <Settings {...props} />,
-  },
-  {
-    path: "/",
-    label: "Logout",  
-    icon: (props: IconBaseProps) => <TbLogout2 {...props} />,
-  },
+    {
+        path: "/dashboard/profile",
+        label: "Profile",
+        icon: (props: IconBaseProps) => <User {...props} />,
+    },
+    {
+        path: "/dashboard/hackathons",
+        label: "Hackathons",
+        icon: (props: IconBaseProps) => <CodeXml {...props} />,
+    },
+    {
+        path: "/dashboard/organizehackathons",
+        label: "Organize a Hackathon",
+        icon: (props: IconBaseProps) => <ListTodo {...props} />,
+    },
+    {
+        path: "/",
+        label: "Settings",
+        icon: (props: IconBaseProps) => <Settings {...props} />,
+    },
+    {
+        path: "/",
+        label: "Logout",
+        icon: (props: IconBaseProps) => <TbLogout2 {...props} />,
+    },
 ];
 
 const Sidebar = () => {
-  const accessToken = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith("accessToken="))
-    ?.split("=")[1];
+    const accessToken = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("accessToken="))
+        ?.split("=")[1];
 
-
-  return (
-    <>
-      <div className="absolute -left-[22px] top-6 -rotate-90 border-[.7rem] border-transparent border-b-white drop-shadow-[0px_-1px_1px_rgba(0,0,0,0.08)]"></div>
-      <div className="sticky top-4 flex h-[calc(100vh-32px)] flex-col justify-between overflow-y-auto px-4 w-fit">
-        <div>
-          <div className="mb-4 mt-2 h-24 cursor-pointer border-b border-slate-300 pb-3 pl-5">
-            <Link to={"/"}>
-              <img
-                src={Logo}
-                alt="Logo"
-                className="h-[100%] object-cover"
-              />
-            </Link>
-          </div>
-          <div className="relative flex flex-col gap-2">
-            {navItems.map((item, index) => (
-              <NavLink
-                key={index}
-                to={item.path}
-                className={({ isActive }) =>
-                  `flex w-full cursor-pointer items-center justify-start gap-4 rounded-md px-4 py-2 transition-[box-shadow,_background-color,_color] ${
-                    isActive
-                      ? "bg-white font-medium text-neutral-950 ring-1 ring-slate-300/20"
-                      : "bg-transparent text-slate-700 shadow-none hover:bg-slate-200"
-                  }`
-                }
-              >
-                {({ isActive }) => (
-                  <>
-                    {item.label === "Analytics" && (
-                      <>
-                        <span
-                          className={`flex h-[1.4rem] w-[1.4rem] items-center justify-center rounded-sm border-b-[2.3px] border-l-[2.3px] ${isActive ? "border-blue-500" : "border-slate-600"}`}
-                        >
-                          <item.icon
-                            className={`m-[1px] h-[1.35rem] w-[1.35rem] ${isActive ? "text-blue-500" : ""}`}
-                          />
-                        </span>
-                        <span onClick={() => {item.label == "Logout" ? localStorage.removeItem("accessToken") : ""}}>{item.label}</span>
-                      </>
-                    )}
-                    {item.label === "Calendar" && (
-                      <>
-                        <span
-                          className={`flex items-center justify-center rounded-sm`}
-                        >
-                          <item.icon
-                            className={`m-[1px] h-[1.45rem] w-[1.45rem] ${isActive ? "text-blue-500" : ""}`}
-                          />
-                        </span>
-                        <span>{item.label}</span>
-                      </>
-                    )}
-                    {item.label !== "Analytics" &&
-                      item.label !== "Calendar" && (
-                        <>
-                          <span
-                            className={`flex items-center justify-center rounded-sm`}
-                          >
-                            <item.icon
-                              className={`m-[1px] h-[1.55rem] w-[1.55rem] ${isActive ? "text-blue-500" : ""}`}
+    return (
+        <>
+            <div className="absolute -left-[22px] top-6 -rotate-90 border-[.7rem] border-transparent border-b-white drop-shadow-[0px_-1px_1px_rgba(0,0,0,0.08)]"></div>
+            <div className="sticky top-4 flex h-[calc(100vh-32px)] w-fit flex-col justify-between overflow-y-auto pl-4 pr-2">
+                <div>
+                    <div className="mb-4 mt-2 h-24 cursor-pointer border-b border-slate-300 pb-3 pl-5">
+                        <Link to={"/"}>
+                            <img
+                                src={Logo}
+                                alt="Logo"
+                                className="h-[100%] object-cover"
                             />
-                          </span>
-                          <span>{item.label}</span>
-                        </>
-                      )}
-                  </>
-                )}
-              </NavLink>
-            ))}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+                        </Link>
+                    </div>
+                    <div className="relative flex flex-col gap-2">
+                        {navItems.map((item, index) => (
+                            <NavLink
+                                key={index}
+                                to={item.path}
+                                className={({ isActive }) =>
+                                    `flex w-full cursor-pointer items-center justify-start gap-4 rounded-md px-4 py-2 transition-[box-shadow,_background-color,_color] ${
+                                        isActive
+                                            ? "bg-white font-medium text-neutral-950 ring-1 ring-slate-300/20"
+                                            : "bg-transparent text-slate-700 shadow-none hover:bg-slate-200"
+                                    }`
+                                }
+                            >
+                                {({ isActive }) => (
+                                    <>
+                                        {item.label === "Analytics" && (
+                                            <>
+                                                <span
+                                                    className={`flex h-[1.4rem] w-[1.4rem] items-center justify-center rounded-sm border-b-[2.3px] border-l-[2.3px] ${isActive ? "border-blue-500" : "border-slate-600"}`}
+                                                >
+                                                    <item.icon
+                                                        className={`m-[1px] h-[1.35rem] w-[1.35rem] ${isActive ? "text-blue-500" : ""}`}
+                                                    />
+                                                </span>
+                                                <span
+                                                    onClick={() => {
+                                                        item.label == "Logout"
+                                                            ? localStorage.removeItem(
+                                                                  "accessToken",
+                                                              )
+                                                            : "";
+                                                    }}
+                                                >
+                                                    {item.label}
+                                                </span>
+                                            </>
+                                        )}
+                                        {item.label === "Calendar" && (
+                                            <>
+                                                <span
+                                                    className={`flex items-center justify-center rounded-sm`}
+                                                >
+                                                    <item.icon
+                                                        className={`m-[1px] h-[1.45rem] w-[1.45rem] ${isActive ? "text-blue-500" : ""}`}
+                                                    />
+                                                </span>
+                                                <span>{item.label}</span>
+                                            </>
+                                        )}
+                                        {item.label !== "Analytics" &&
+                                            item.label !== "Calendar" && (
+                                                <>
+                                                    <span
+                                                        className={`flex items-center justify-center rounded-sm`}
+                                                    >
+                                                        <item.icon
+                                                            className={`m-[1px] h-[1.55rem] w-[1.55rem] ${isActive ? "text-blue-500" : ""}`}
+                                                        />
+                                                    </span>
+                                                    <span>{item.label}</span>
+                                                </>
+                                            )}
+                                    </>
+                                )}
+                            </NavLink>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Sidebar;
--- Rubix/src/layout/DashboardLayout.tsx
+++ Rubix/src/layout/DashboardLayout.tsx
@@ -3,19 +3,20 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 
 const DashboardLayout = () => {
-  return (
-    <>
-      <div
-        className={`grid min-h-screen grid-cols-[300px,1fr] overflow-clip rounded-md bg-slate-100`}
-      >
-        <div className="mr-2">
-          <Sidebar />
-        </div>
-
-        <Outlet />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div
+                className={`grid min-h-screen grid-cols-[240px,1fr] overflow-clip rounded-md bg-slate-100`}
+            >
+                <div className="mr-2">
+                    <Sidebar />
+                </div>
+                <div className="max-h-[calc(100vh-1rem) mt-[0.5rem]] overflow-y-scroll rounded-md bg-white">
+                    <Outlet />
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default DashboardLayout;
